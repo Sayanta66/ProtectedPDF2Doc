483b9572af7dcfbc1dd29d129e2ca5de5c056f47
Sayanta Banerjee <53281158+Sayanta66@users.noreply.github.com>
2021-11-07 19:49:58 +0530

    Add files via upload

---
+++ pdfconverter/urls.py
@@ -0,0 +1,42 @@
+"""pdfconverter URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from os import name
+from django.contrib import admin
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from .pdfapp import views
+from django.views.static import serve
+from django.conf.urls import url
+from django.urls import path
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.uploadfile, name='uploadfile'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    path('', include('social_django.urls')),
+    path('profile/', views.profile),
+    path('', include('django.contrib.auth.urls')),
+    path('profile/', views.profile),
+    path('logout/', views.logout),
+]
+
+
+if settings.DEBUG:
+    urlpatterns == static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
